494109c3cd0e450fc9efe3dfd810f9b270a6daa6
Lasttrader <49299104+Lasttrader@users.noreply.github.com>
2023-05-15 20:54:09 +0300

    update E4_hw/2.js

--- E4_hw/2.js
+++ E4_hw/2.js
@@ -2,3 +2,22 @@
 //Задание 2.
 /*Написать функцию, которая принимает в качестве аргументов строку и объект,
 а затем проверяет есть ли у переданного объекта свойство с данным именем.Функция должна возвращать true или false.*/
+
+let test_object = {
+  'key1':1,
+  'key2':2
+};
+let test_string = 'key2'
+
+function checkObject(my_strig, my_object){
+  for (let key in my_object) {
+    if (key === my_strig) {
+        console.log('True', ' for ', my_strig);
+       }
+    else {
+      console.log('False');
+    }
+  }
+};
+
+checkObject(test_string, test_object);
