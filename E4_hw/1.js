ec10b75db48d5766d1c50bd408e7e3316fd76055
Lasttrader <49299104+Lasttrader@users.noreply.github.com>
2023-05-15 20:28:12 +0300

    update E4_hw/1.js

--- E4_hw/1.js
+++ E4_hw/1.js
@@ -1,3 +1,21 @@
 //Задание 1.
 /*Написать, функцию, которая принимает в качестве аргумента объект и выводит в консоль все ключи и значения только собственных свойств.
 Данная функция не должна возвращать значение.*/
+
+function getObject(obj_dict){
+  for (let key in obj_dict) {
+    if (obj_dict.hasOwnProperty(key)) {
+        console.log(key);
+    }
+}
+}
+
+let me = {
+  1:'key1',
+  2:'key2'
+};
+
+let student = Object.create(me);
+student.age = 37;
+
+getObject(grisha);
